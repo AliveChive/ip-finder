1cc80963960e8319fe4c56ac000871a115d425d2
mrpodato <41372461+mrpodato@users.noreply.github.com>
2019-03-02 16:05:34 +0800

    Add files via upload

---
+++ ip finder.py
@@ -0,0 +1,12 @@
+import requests
+import pyperclip
+src = requests.get('http://checkip.dyndns.org/').text
+src = src = src[76:].replace('</body></html>','').rstrip()
+pyperclip.copy(src)
+print (src + " Is Your IP.")
+print ('Press c to copy')
+choice = input ()
+if choice.lower() == 'c':
+    pyperclip.copy(src)
+    print ('Copied')
+input()
