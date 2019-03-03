10046bf7117bdeb948e91ca71f2d986d8e8953ce
mrpodato <41372461+mrpodato@users.noreply.github.com>
2019-03-03 22:14:31 +0800

    copy bug
    fixed a glitch so it dosent copy your ip adress before you press c

--- ip finder.py
+++ ip finder.py
@@ -2,7 +2,6 @@ import requests
 import pyperclip
 src = requests.get('http://checkip.dyndns.org/').text
 src = src = src[76:].replace('</body></html>','').rstrip()
-pyperclip.copy(src)
 print (src + " Is Your IP.")
 print ('Press c to copy')
 choice = input ()
